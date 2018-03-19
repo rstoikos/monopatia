c42fb664703d6abfe9b6be1f080af374d1abc249
rstoikos <rstoikos@hotmail.com>
2018-03-19 12:26:20 +0200

    fixed map.html

---
+++ public/javascripts/calcroute.js
@@ -0,0 +1,12 @@
+function calcRoute() {
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'WALKING'
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+       directionsDisplay.setDirections(result);
+    }
+    
+  });
